0441c367764a7ee61236385f8d296c7e8c494105
Sam <sam@rosss.dev>
2024-03-22 15:00:54 -0500

    file layout fix

--- app/server.js
+++ app/server.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('App is listening on port 3000');
+});
 
 // Static files.
 app.use('/models', express.static('models'));
